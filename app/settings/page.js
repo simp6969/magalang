d1107c1a0cd92d467d6f25c1e5433edb8f62717f
ariuka <ariunboldbold200@gmail.com>
2023-12-11 20:25:09 +0800

    something added

--- app/settings/page.js
+++ app/settings/page.js
@@ -4,21 +4,23 @@ import { useState } from "react";
 import { Data } from "../components/Data";
 import Image from "next/image";
 export default function App() {
-  const [BaseData, setBaseData] = useState();
   const uniqueMap = {};
-  const data = Data.filter((element) => {
-    const key = element.path; // Change this to the property you want to use as the key
-    const isNew = !uniqueMap[key];
-    if (isNew) {
-      uniqueMap[key] = true;
-    }
-    return isNew;
-  });
+  const [data, setData] = useState(
+    Data.filter((element) => {
+      const key = element.path;
+      const isNew = !uniqueMap[key];
+      if (isNew) {
+        uniqueMap[key] = true;
+      }
+      return isNew;
+    })
+  );
   const handleFile = (e) => {
     var fileReader = new FileReader();
     fileReader.onload = function (fileLoadedEvent) {
       var srcData = fileLoadedEvent.target.result;
-      setBaseData({ ...BaseData, srcData });
+      setData([{ ...data, srcData }]);
+      console.log([...data]);
     };
     try {
       fileReader.readAsDataURL(e.target.files[0]);
@@ -26,46 +28,42 @@ export default function App() {
       console.log("return by death");
     }
   };
-  function handlePhoto(e) {
-    setBaseData({ ...BaseData, click: true, id: e });
-  }
-  console.log(data);
+
   return (
-    <div className="scroll flex justify-center items-center h-[100vh] w-[100vw] flex-wrap gap-[10px]">
+    <div className="overflow-auto  flex justify-center items-center h-[100vh] w-[100vw] flex-wrap gap-[10px]">
       {data.map((element, index) => {
         return (
-          <div
-            className="relative w-[125px] h-[200px]"
-            onClick={() => handlePhoto(index)}
-            key={index}
-          >
-            <Image src={element.path} height={150} width={150} />
+          <div className="flex flex-col gap-5" key={index}>
+            {element?.srcData ? (
+              <Image
+                alt="photo"
+                width={120}
+                height={170}
+                src={element?.srcData}
+              ></Image>
+            ) : (
+              <Image
+                onClick={() => document.getElementById("getFile").click()}
+                alt="photo"
+                src={element.path}
+                height={190}
+                width={160}
+              />
+            )}
+
+            <button onClick={() => document.getElementById("getFile").click()}>
+              change photo
+            </button>
             <input
-              style={
-                BaseData?.click ? { display: "block" } : { display: "none" }
-              }
               accept="image/*"
-              type="file"
               onChange={handleFile}
+              type="file"
+              id="getFile"
+              style={{ display: "none" }}
             ></input>
-            <Image
-              style={
-                BaseData?.srcData ? { display: "block" } : { display: "none" }
-              }
-              width={100}
-              height={150}
-              src={BaseData?.srcData}
-            ></Image>
           </div>
         );
       })}
-      {/* <input
-        id="image"
-        onChange={handleFile}
-        type="file"
-        accept="images"
-      ></input>
-      <label for="image">hello</label>*/}
     </div>
   );
 }
