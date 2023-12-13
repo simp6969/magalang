545da9705b333f03d851c570175b14411449c8e9
ariuka <ariunboldbold200@gmail.com>
2023-12-13 20:59:37 +0800

    still fixing settings

--- app/settings/page.js
+++ app/settings/page.js
@@ -3,67 +3,64 @@
 import { useState } from "react";
 import { Data } from "../components/Data";
 import Image from "next/image";
+
 export default function App() {
-  const uniqueMap = {};
-  const [data, setData] = useState(
-    Data.filter((element) => {
-      const key = element.path;
-      const isNew = !uniqueMap[key];
-      if (isNew) {
-        uniqueMap[key] = true;
-      }
-      return isNew;
-    })
-  );
-  const handleFile = (e) => {
+  const [clickedPhoto, setClickedPhoto] = useState();
+  const [baseData, setBaseData] = useState(() => {
+    const unique = Data.filter((obj, index) => {
+      return index === Data.findIndex((o) => obj.path === o.path);
+    });
+    return unique;
+  });
+  function handleClick(id) {
+    document.getElementById("input").click();
+    setClickedPhoto(id);
+  }
+  function handleFile(event) {
+    console.log(event);
     var fileReader = new FileReader();
     fileReader.onload = function (fileLoadedEvent) {
       var srcData = fileLoadedEvent.target.result;
-      setData([{ ...data, srcData }]);
-      console.log([...data]);
+      console.log(srcData);
+      const index = baseData.filter((element) => {
+        if (element.id === clickedPhoto) {
+          element.path = srcData;
+          return element;
+        }
+      });
     };
     try {
-      fileReader.readAsDataURL(e.target.files[0]);
+      fileReader.readAsDataURL(event.target.files[0]);
     } catch {
       console.log("return by death");
     }
-  };
-
+  }
   return (
-    <div className="overflow-auto  flex justify-center items-center h-[100vh] w-[100vw] flex-wrap gap-[10px]">
-      {data.map((element, index) => {
-        return (
-          <div className="flex flex-col gap-5" key={index}>
-            {element?.srcData ? (
+    <div className="flex h-[100vh] w-[100vw] justify-center flex-col items-center">
+      <h1>editing photo</h1>
+      <div className="flex">
+        {baseData.map((element, index) => {
+          return (
+            <div className="h-[300px] gap-[10px] p-[5px] flex" key={index}>
               <Image
-                alt="photo"
-                width={120}
-                height={170}
-                src={element?.srcData}
-              ></Image>
-            ) : (
-              <Image
-                onClick={() => document.getElementById("getFile").click()}
-                alt="photo"
+                alt="phots"
+                width="0"
+                height="0"
+                sizes="100vw"
+                className="w-full h-auto"
                 src={element.path}
-                height={190}
-                width={160}
-              />
-            )}
-
-            <button onClick={() => document.getElementById("getFile").click()}>
-              change photo
-            </button>
-            <input
-              accept="image/*"
-              onChange={handleFile}
-              type="file"
-              id="getFile"
-              style={{ display: "none" }}
-            ></input>
-          </div>
-        );
-      })}
+                onClick={() => handleClick(element.id)}
+              ></Image>
+            </div>
+          );
+        })}
+        <input
+          type="file"
+          className="hidden"
+          id="input"
+          onChange={handleFile}
+        ></input>
+      </div>
     </div>
   );
 }
